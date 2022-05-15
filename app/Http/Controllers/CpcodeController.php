9305e8c6b172fb9736e21467a3ee1146ff517d56
unknown <pruebanrj@hotmail.com>
2022-05-14 21:52:13 -0300

    ultimos cambios

--- app/Http/Controllers/CpcodeController.php
+++ app/Http/Controllers/CpcodeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cpcode;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
@@ -13,18 +14,24 @@ class CpcodeController extends Controller
     public function detalleAsenta(Request $request)
     { 
         $sel = Cpcode::where('d_codigo',$request->codigo)->get();
-        $federal = ['key' => $sel[0]->c_estado,'name'=>$sel[0]->d_estado,'code'=>$sel[0]->c_CP];
+       
+        if(!isset($sel[0])){
+            return response()->json([
+                'resp' => 'no hay nada'
+            ]);
+        }
+        $federal = ['key' => $sel[0]->c_estado,'name'=>Str::upper($sel[0]->d_estado),'code'=>$sel[0]->c_CP];
         
-        $asenta = ['zip_code'=>$sel[0]->d_codigo,'locality'=> $sel[0]->d_ciudad,
+        $asenta = ['zip_code'=>$sel[0]->d_codigo,'locality'=> Str::upper($sel[0]->d_ciudad),
       
                    'federal_entity'=>$federal,
-                   'settlements' => ['key'=>$sel[0]->id_asenta_cpcons,'name' => $sel[0]->d_asenta,'zone_type'=>$sel[0]->d_zona,
-                                    'settlement_type' => ['name' => $sel[0]->d_tipo_asenta]],
-                   'municipality' => ['key' => $sel[0]->c_mnpio,'name'=>$sel[0]->D_mnpio]
+                   'settlements' => array(['key'=>$sel[0]->id_asenta_cpcons,'name' => Str::upper($sel[0]->d_asenta),'zone_type'=>Str::upper($sel[0]->d_zona),
+                                    'settlement_type' => ['name' => Str::upper($sel[0]->d_tipo_asenta)]]),
+                   'municipality' => ['key' => $sel[0]->c_mnpio,'name'=>Str::upper($sel[0]->D_mnpio)]
                                 
     ];
-        // dd($asenta);
+        
        
-        return $asenta;
+        return response()->json($asenta);
     }
 }
